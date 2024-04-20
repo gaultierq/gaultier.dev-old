1347db8baf4f16e8536a45845a1cbc3b2ec179fc
Quentin Gaultier <quentin.gaultier@gmail.com>
2024-04-20 14:14:28 +0200

    resume ++

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,20 @@ module.exports = {
     "./source/**/*.{html,erb,slim}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#FEFEFE"
+      },
+      fontSize: {
+        xs: '0.5rem',
+        sm: '0.75rem',
+        base: '0.9rem',
+        xl: '1rem',
+        '2xl': '1.4rem',
+        '3xl': '1.7rem',
+      },
+    },
   },
   plugins: [],
-}
+};
 
