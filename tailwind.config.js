698991b3869aebcd653dbc07f27eff9011af59fa
Quentin Gaultier <quentin.gaultier@gmail.com>
2024-04-22 17:16:31 +0200

    resume ++

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
       },
       fontSize: {
         xs: '0.5rem',
-        sm: '0.75rem',
-        base: '0.9rem',
+        sm: '0.7rem',
+        base: '0.85rem',
         xl: '1rem',
         '2xl': '1.4rem',
         '3xl': '1.7rem',
